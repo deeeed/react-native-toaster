31226774d3d9654bde43cff18e0fe177cfe54fb3
abretonc7s <arthur.breton@consensys.net>
2023-09-20 15:35:28 +0800

    feat: update example and dependencies to display on web

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,14 +1,87 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
+import { ToastProvider, useToast } from '@siteed/react-native-toaster';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-paper';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-export default function App() {
-  React.useEffect(() => {}, []);
+const App = () => {
+  const toaster = useToast();
 
   return (
     <View style={styles.container}>
       <Text>TODO</Text>
+      <View>
+        <Button
+          onPress={() => {
+            toaster.show({
+              message: 'Save Success',
+              subMessage: 'Please check the application',
+              type: 'success',
+              actionLabel: 'OK',
+              iconVisible: true,
+              snackbarStyle: {
+                // backgroundColor: 'red',
+              },
+              duration: 100000,
+              action() {
+                console.log('ok');
+              },
+            });
+          }}
+        >
+          Success Toast
+        </Button>
+        <Button
+          onPress={() => {
+            toaster.show({
+              message: 'This is a toast',
+              type: 'info',
+              actionLabel: 'OK',
+              duration: 10000,
+              action() {
+                console.log('ok');
+              },
+            });
+          }}
+        >
+          Info Toast
+        </Button>
+        <Button
+          onPress={() => {
+            toaster.show({
+              message: 'This is a toast',
+              type: 'warning',
+              iconVisible: true,
+            });
+          }}
+        >
+          Warning Toast
+        </Button>
+        <Button
+          onPress={() => {
+            toaster.show({
+              message: 'This is a toast',
+              type: 'error',
+              iconVisible: true,
+              position: 'middle',
+            });
+          }}
+        >
+          Error Toast
+        </Button>
+      </View>
     </View>
+  );
+};
+
+export default function WithToast() {
+  return (
+    <SafeAreaProvider>
+      <ToastProvider>
+        <App />
+      </ToastProvider>
+    </SafeAreaProvider>
   );
 }
 
